591e0ece60511b55ca99821431bbfb7e261f4317
Javier Camarena <jacc_123@hotmail.com>
2021-10-20 22:10:53 -0500

    added basic configuration

---
+++ app/src/main/java/com/example/userappchallenge/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.userappchallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.userappchallenge.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
